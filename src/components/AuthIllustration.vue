8bd060a46ea74e8bfa44f92f1b086955f2cac82e
conarti <abelous2009@gmail.com>
2022-07-26 17:56:10 +0300

    feat(AuthIllustration): add blured squares

--- src/components/AuthIllustration.vue
+++ src/components/AuthIllustration.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="auth-illustration">
+    <AuthIllustrationSquare position="1" size="smallest" />
+    <AuthIllustrationSquare position="2" size="small" />
+    <AuthIllustrationSquare position="3" size="medium" />
+    <AuthIllustrationSquare position="4" size="big" />
+    <AuthIllustrationSquare position="5" size="bigger" />
+    <AuthIllustrationSquare position="6" size="biggest" />
+
     <AuthIllustrationIcon icon="alarm" size="smallest" />
     <AuthIllustrationIcon icon="lock" size="small" />
     <AuthIllustrationIcon icon="chat" size="medium" />
@@ -10,11 +17,13 @@
 
 <script>
 import AuthIllustrationIcon from '@/components/AuthIllustrationIcon.vue';
+import AuthIllustrationSquare from '@/components/AuthIllustrationSquare.vue';
 
 export default {
   name: 'AuthIllustration',
   components: {
     AuthIllustrationIcon,
+    AuthIllustrationSquare,
   },
 };
 </script>
---
+++ src/components/AuthIllustrationSquare.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="auth-illustration-square"
+    :class="[
+      `auth-illustration-square--is-${size}`,
+      `auth-illustration-square--is-position-${position}`,
+     ]"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: 'AuthIllustrationSquare',
+  props: {
+    size: {
+      type: String,
+      required: true,
+    },
+    position: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
