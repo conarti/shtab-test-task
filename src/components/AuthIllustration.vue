94c5d173ff727b6644a0526341571b7377047169
conarti <abelous2009@gmail.com>
2022-07-26 18:09:13 +0300

    feat(AuthIllustration): add ellipses

--- src/components/AuthIllustration.vue
+++ src/components/AuthIllustration.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="auth-illustration">
+    <AuthIllustrationEllipse size="small" />
+    <AuthIllustrationEllipse size="medium" />
+    <AuthIllustrationEllipse size="big" />
+    <AuthIllustrationEllipse size="biggest" />
+
     <AuthIllustrationSquare size="smallest" />
     <AuthIllustrationSquare size="small" />
     <AuthIllustrationSquare size="medium" />
@@ -7,21 +12,25 @@
     <AuthIllustrationSquare size="bigger" />
     <AuthIllustrationSquare size="biggest" />
 
-    <AuthIllustrationIcon icon="alarm" size="smallest" />
-    <AuthIllustrationIcon icon="lock" size="small" />
-    <AuthIllustrationIcon icon="chat" size="medium" />
-    <AuthIllustrationIcon icon="fire" size="big" />
-    <AuthIllustrationIcon icon="shtab-logo" size="biggest" />
+    <div class="auth-illustration-icons">
+      <AuthIllustrationIcon icon="alarm" size="smallest" />
+      <AuthIllustrationIcon icon="lock" size="small" />
+      <AuthIllustrationIcon icon="chat" size="medium" />
+      <AuthIllustrationIcon icon="fire" size="big" />
+      <AuthIllustrationIcon icon="shtab-logo" size="biggest" />
+    </div>
   </div>
 </template>
 
 <script>
+import AuthIllustrationEllipse from '@/components/AuthIllustrationEllipse.vue';
 import AuthIllustrationIcon from '@/components/AuthIllustrationIcon.vue';
 import AuthIllustrationSquare from '@/components/AuthIllustrationSquare.vue';
 
 export default {
   name: 'AuthIllustration',
   components: {
+    AuthIllustrationEllipse,
     AuthIllustrationIcon,
     AuthIllustrationSquare,
   },
---
+++ src/components/AuthIllustrationEllipse.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="auth-illustration-ellipse"
+    :class="`auth-illustration-ellipse--is-${size}`"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: 'AuthIllustrationEllipse',
+  props: {
+    size: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
