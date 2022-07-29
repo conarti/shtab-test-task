73df74def61e05c03176b1c734bc678ac080bd25
conarti <abelous2009@gmail.com>
2022-07-29 21:44:22 +0300

    feat(LoginView): change 'div' to 'form', add inputs, add button type submit

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="login">
+  <form class="login" @submit.prevent>
     <h1 class="login-title">Авторизация</h1>
-    <app-button>Войти</app-button>
-  </div>
+
+    <AppInput class="login-input" label="Email" type="email" />
+    <AppInput class="login-input" label="Пароль" type="password" />
+
+    <app-button type="submit">Войти</app-button>
+  </form>
 </template>
 
 <script>
 import AppButton from '@/components/AppButton.vue';
+import AppInput from '@/components/AppInput.vue';
 
 export default {
   name: 'LoginView',
   components: {
     AppButton,
+    AppInput,
   },
 };
 </script>
