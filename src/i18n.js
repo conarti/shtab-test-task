0daf99bc767446b3b70a3016cda76312704dd2fd
conarti <abelous2009@gmail.com>
2022-07-30 12:39:16 +0300

    feat: add i18n to project

---
+++ src/i18n.js
@@ -0,0 +1,10 @@
+import { createI18n } from 'vue-i18n';
+import ru from '@/messages/ru';
+
+export default createI18n({
+  locale: 'ru',
+  fallbackLocale: 'ru',
+  messages: {
+    ru,
+  },
+});
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import './styles/index.scss';
 import { createApp } from 'vue';
+import i18n from './i18n';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 require.context('./assets/icons', true, /\.svg$/); // import all icons for webpack
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(i18n)
+  .mount('#app');
