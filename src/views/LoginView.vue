d84e9508f4c6cbc9c176306ad92258d7002b8553
conarti <abelous2009@gmail.com>
2022-07-31 10:31:06 +0300

    feat(LoginView): add login method and display errors of fields validation

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,20 +1,24 @@
 <template>
   <form
     class="login-view"
-    @submit.prevent
+    @submit.prevent="login"
   >
     <h1 class="login-view-title">
       {{ $t('loginView.title') }}
     </h1>
 
     <AppInput
+      v-model="username"
       class="login-view-input"
       :label="$t('loginView.inputLabels.email')"
+      :errors="errors.username"
       type="email"
     />
     <AppInput
+      v-model="password"
       class="login-view-input"
       :label="$t('loginView.inputLabels.password')"
+      :errors="errors.password"
       type="password"
     />
 
@@ -33,6 +37,32 @@ export default {
   components: {
     AppButton,
     AppInput,
+  },
+  data() {
+    return {
+      username: null,
+      password: null,
+      errors: {
+        username: [],
+        password: [],
+      },
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        await this.$store.dispatch('auth/login', {
+          username: this.username,
+          password: this.password,
+        });
+        this.$router.push({ name: 'profile' });
+      } catch (error) {
+        const { password, username } = error.response.data;
+
+        this.errors.password = password ?? [];
+        this.errors.username = username ?? [];
+      }
+    },
   },
 };
 </script>
