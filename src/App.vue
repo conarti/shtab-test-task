ebade0da223f10c1dd6d9dcc0ee5451e281fe280
conarti <abelous2009@gmail.com>
2022-08-01 14:24:24 +0300

    feat: create AppAlerts.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <router-view />
+  <AppAlerts />
 </template>
 
 <script>
+import AppAlerts from '@/components/AppAlerts.vue';
+
 export default {
   name: 'App',
+  components: {
+    AppAlerts,
+  },
 };
 </script>
---
+++ src/components/AppAlerts.vue
@@ -0,0 +1,32 @@
+<template>
+  <teleport to="body">
+    <transition-group
+      tag="div"
+      name="alerts-list"
+      class="app-alerts"
+    >
+      <div
+        v-for="alert in allAlerts"
+        :key="alert.id"
+        class="app-alerts-alert"
+      >
+        <p class="app-alerts-alert-message">
+          {{ alert.message }}
+        </p>
+      </div>
+    </transition-group>
+  </teleport>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'AppAlerts',
+  computed: {
+    ...mapGetters('alerts', {
+      allAlerts: 'all',
+    }),
+  },
+};
+</script>
