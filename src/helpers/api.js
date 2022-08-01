4dff2b34b8e140f0b81686d20ae53e0a5458d87c
conarti <abelous2009@gmail.com>
2022-08-01 13:46:37 +0300

    refactor(api): init interceptor in api module

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -4,15 +4,19 @@ const apiInstance = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
 });
 
-const createSetAuthInterceptor = (hasAuth, token) => (config) => {
-  if (hasAuth()) {
-    // eslint-disable-next-line no-param-reassign
-    config.headers.Authorization = `Token ${token()}`;
-  } else {
-    // eslint-disable-next-line no-param-reassign
-    delete config.headers.Authorization;
-  }
-  return config;
+const createSetAuthInterceptor = (hasAuth, token) => {
+  const authInterceptor = (config) => {
+    if (hasAuth()) {
+      // eslint-disable-next-line no-param-reassign
+      config.headers.Authorization = `Token ${token()}`;
+    } else {
+      // eslint-disable-next-line no-param-reassign
+      delete config.headers.Authorization;
+    }
+    return config;
+  };
+
+  apiInstance.interceptors.request.use(authInterceptor);
 };
 
 export {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './styles/index.scss';
 import { createApp } from 'vue';
-import api, { createSetAuthInterceptor } from './helpers/api';
+import { createSetAuthInterceptor } from './helpers/api';
 import i18n from './i18n';
 import App from './App.vue';
 import router from './router';
@@ -10,9 +10,7 @@ require.context('./assets/icons', true, /\.svg$/); // import all icons for webpa
 
 const hasAuth = () => store.getters['auth/hasAuth'];
 const token = () => store.getters['auth/token'];
-const setAuthCb = createSetAuthInterceptor(hasAuth, token);
-
-api.interceptors.request.use(setAuthCb);
+createSetAuthInterceptor(hasAuth, token);
 
 createApp(App)
   .use(store)
